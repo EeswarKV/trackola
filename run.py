a69feebe8d1582e5b445d6dfd09c0da4d01e615c
eshwar4299 <eshwar4299@gmail.com>
2023-08-22 10:50:03 +0200

    feat: add login and general setting call

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from multiprocessing import Process
 from services.mock_server import app as mock_app  # import the Flask instance of the mock server
 import importlib
-from portfolio_builder.app import app  # assuming app is the Flask instance in your app.py
+from src.app import app  # assuming app is the Flask instance in your app.py
 
 def run_app():
     app.run(port=5000)
